dcb36fb34f71f056421a26cf1620a3033bfb58a2
NANAME002 <dennzerogo@gmail.com>
2024-10-25 12:40:22 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////n1
+            //Random end= new Random();
+            //int a = end.Next(10, 99);
+            //Console.WriteLine($"Случайное число = {a}");
+            //string x = (a % 2 == 0) ? "четное" : "нечетное";
+            //Console.WriteLine(x);
+
+            //n2-3
+            //Random end = new Random();
+            //int a = end.Next(1, 7);
+            //switch (a)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Понедельник");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Вторник");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Среда");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Четверг");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Пятница");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Суббота");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Воскресенье");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Неверный номер. Пожалуйста, введите число от 1 до 7.");
+            //        break;
+            //}
+
+
+            //n4
+            //Console.Write("Введи число от 1 до 12");
+            //int a = int.Parse(Console.ReadLine());
+            //switch (a)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Январь");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Февраль");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Март");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Апрель");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Май");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Июнь");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Июль");
+            //        break;
+            //    case 8:
+            //        Console.WriteLine("Август");
+            //        break;
+            //    case 9:
+            //        Console.WriteLine("Сентябрь");
+            //        break;
+            //    case 10:
+            //        Console.WriteLine("Октябрь");
+            //        break;
+            //    case 11:
+            //        Console.WriteLine("Ноябрь");
+            //        break;
+            //    case 12:
+            //        Console.WriteLine("Декабрь");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Неверный номер. Пожалуйста, введите число от 1 до 7.");
+            //        break;
+            //}
+
+            //n5
+            //Console.Write("Введите признак а,в,м,с,п - ");
+            //char a = Console.ReadKey().KeyChar;
+            //switch (a)
+            //{
+            //    case 'а':
+            //        Console.WriteLine("330 км/ч");
+            //        break;
+            //    case 'в':
+            //        Console.WriteLine("35 км/ч");
+            //        break;
+            //    case 'м':
+            //        Console.WriteLine("300 км/ч");
+            //        break;
+            //    case 'с':
+            //        Console.WriteLine("2000 км/ч");
+            //        break;
+            //    case 'п':
+            //        Console.WriteLine("660 км/ч");
+            //        break;
+            //}
+
+            //n5
+
+            //Console.Write("Введите признак ф,м,и,г,б - ");
+            //char a = Console.ReadKey().KeyChar;
+            //switch (a)
+            //{
+            //    case 'а':
+            //        Console.WriteLine("физика");
+            //        break;
+            //    case 'в':
+            //        Console.WriteLine("математика");
+            //        break;
+            //    case 'м':
+            //        Console.WriteLine("история");
+            //        break;
+            //    case 'с':
+            //        Console.WriteLine("география");
+            //        break;
+            //    case 'п':
+            //        Console.WriteLine("биология");
+            //        break;
+            //}
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
